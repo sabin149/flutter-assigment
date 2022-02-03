20985ed734ea467b04494e787a879fd7b8417ab2
sabin149 <dangalsabin149@gmail.com>
2022-02-03 23:22:12 +0545

    added dark mode feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './login_screen.dart';
+import './theme.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences.getInstance().then((prefs) {
+    var isDarkTheme = prefs.getBool("darkTheme") ?? false;
+    return runApp(
+      ChangeNotifierProvider<ThemeProvider>(
+        child: const MyApp(),
+        create: (BuildContext context) {
+          return ThemeProvider(isDarkTheme);
+        },
+      ),
+    );
+  });
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-   
-    return Scaffold(
-      appBar: AppBar(
-      
-        title:const Text("main page"),
-      ),
-body:const Text("hello world")
+    return Consumer<ThemeProvider>(
+      builder: (context, value, child) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme: value.getTheme(),
+          home: const LoginScreen(),
+        );
+      },
     );
   }
 }
